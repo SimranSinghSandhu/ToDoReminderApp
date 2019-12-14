e0d45aa1f4b19c1ee32c0a85093ec39fa44eb12f
Simran <sam.sandhu131@gmail.com>
2019-12-14 10:59:56 +0530

    First Commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoReminderApp
+//
+//  Created by Simran Singh Sandhu on 14/12/19.
+//  Copyright © 2019 Simran Singh Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
