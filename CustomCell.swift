d94a3e3166a8c3e2a109410b3cafa6a77dd62749
Simran <sam.sandhu131@gmail.com>
2019-12-17 18:00:18 +0530

    Creating TextField inside Custom Cell

--- CustomCell.swift
+++ CustomCell.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class CustomCell: UITableViewCell {
     
+    let cellTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter New Item..."
+        return textField
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        settingTextField()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    
+    private func settingTextField() {
+        
+        self.addSubview(cellTextField)
+        
+        cellTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5).isActive = true
+        cellTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5).isActive = true
+        cellTextField.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0).isActive = true
+        cellTextField.heightAnchor.constraint(equalToConstant: frame.height).isActive = true
     }
     
 }
