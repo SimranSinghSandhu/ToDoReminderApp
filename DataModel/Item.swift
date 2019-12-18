025a5f9f02dfa419f58ff4a01f57a17c038a701a
Simran <sam.sandhu131@gmail.com>
2019-12-18 14:39:01 +0530

    Creating Data Model Class

---
+++ DataModel/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ToDoReminderApp
+//
+//  Created by Simran Singh Sandhu on 17/12/19.
+//  Copyright © 2019 Simran Singh Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    
+    var title: String?
+    var done: Bool?
+}
---
+++ Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  ToDoReminderApp
+//
+//  Created by Simran Singh Sandhu on 17/12/19.
+//  Copyright © 2019 Simran Singh Sandhu. All rights reserved.
+//
+
+import Foundation
